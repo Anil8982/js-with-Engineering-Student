04e53ec834b649149c4d8cf4e57cf3be3ee7b83f
Anil8982 <103114838+Anil8982@users.noreply.github.com>
2024-01-05 07:40:32 +0000

    container config done

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) // we are using nodejs not browser
+console.log(3+3)
+console.log("anil")
+let name=("anil")
+let age=20
+let isLoggedIn=false
+let state;
+let st=null
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
+
+/*
+JavaScript has 8 Datatypes
+String.
+Number.
+Bigint.
+Boolean.
+Undefined.
+Null.
+Symbol.=unique
+Object.
+*/
---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,27 @@
+/*
+Variables are Containers for Storing Data
+JavaScript Variables can be declared in 4 ways:
+
+Automatically
+Using var
+Using let
+Using const
+*/
+// Using var
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log(typeof x);
+
+
+// Using let
+let x1 = 5;
+let y1 = 6;
+let z1 = x1 + y1;
+console.log(typeof x1);
+
+// Using const
+const x2 = 5;
+const y2 = 6;
+const z2 = x2 + y2;
+console.log(typeof x2);
